633537ef7c57681c2fa812f4df312d0ba2f1c936
minh711 <minhtd.developer@gmail.com>
2024-05-20 21:04:18 +0700

    DNS Demo

---
+++ ConsoleApp/DNSDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ConsoleApp
+{
+    internal class DNSDemo
+    {
+        public void Start()
+        {
+            Console.WriteLine(new string('*', 30));
+            var domainEntry = Dns.GetHostEntry("www.contoso.com");
+            Console.WriteLine(domainEntry.HostName);
+            foreach (var ip in domainEntry.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+            Console.WriteLine(new string('*', 30));
+            var domainEntryByAddress = Dns.GetHostEntry("127.0.0.1");
+            Console.WriteLine(domainEntryByAddress.HostName);
+            foreach (var ip in domainEntryByAddress.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+            Console.ReadLine();
+        }
+    }
+}
